0f1a0650899f89f591e62a28695152dc881801d3
Pascal <pbienenstein@posteo.de>
2024-03-21 19:36:25 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,53 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 
 const App: FC = () => {
     const [data, setData] = useState<any>(null);
     const [eingabe, setEingabe] = useState<string>("");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (eingabe.trim() !== "") { // Überprüfung, ob 'eingabe' leer ist
-                    const response = await fetch(`https://supporter.kulturkosmos.de/api/self-service/shifts/${eingabe}`);
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const jsonData = await response.json();
-                    setData(jsonData);
+
+    const fetchData = async () => {
+        try {
+            if (eingabe.trim() !== "") { // Überprüfung, ob 'eingabe' leer ist
+                const ausgabe = await fetch(`https://supporter.kulturkosmos.de/api/self-service/shifts/${eingabe}`);
+                if (!ausgabe.ok) {
+                    throw new Error('Fehler');
                 }
-            } catch (error) {
-                console.error('Error fetching data:', error);
+                const jsonData = await ausgabe.json();
+                setData(jsonData);
+                console.log(jsonData);
             }
-        };
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
+    const handleClick = () => {
         fetchData();
-    }, [eingabe]); // useEffect abhängig von 'eingabe'
-
-    const handleSubmitEingabe = () => {
-        console.log("HAllo WELT")
-    }
+    };
 
     return (
         <div>
-            <h1>Abfrage Supporter</h1>
+            <h1>Abfrage Supporter {eingabe}</h1>
             {data && (
                 <div>
                     <p>Andere Daten: {JSON.stringify(data)}</p>
+                    <p>Test 1: {data.startAt}</p>
+                    {/*<p>{data}</p>*/}
                 </div>
             )}
 
-            <div onSubmit={handleSubmitEingabe}>
+            <div>
                 <input
                     type="text"
                     placeholder="ID"
-                    value={eingabe} // value statt onChange
+                    value={eingabe}
                     onChange={(e) => setEingabe(e.target.value)}
                 />
-                <button type={"submit"}>Check</button>
-
+                <button onClick={handleClick}>Abfragen</button>
             </div>
-                    </div>
+        </div>
     );
 };
 
 export default App;
+
