50a73644ba3e8626a2604ad71f0c89f54b02046b
Pascal <pbienenstein@posteo.de>
2024-03-21 17:18:30 +0100

    erster code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import React, {FC} from 'react';
-
+import React, { FC, useEffect, useState } from 'react';
 
 const App: FC = () => {
-  return(
-      <div>
-        <h1>Hallo</h1>
-      </div>
-  )
-}
+    const [data, setData] = useState<any>(null);
+    const [eingabe, setEingabe] = useState<string>("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (eingabe.trim() !== "") { // Überprüfung, ob 'eingabe' leer ist
+                    const response = await fetch(`https://supporter.kulturkosmos.de/api/self-service/shifts/${eingabe}`);
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    const jsonData = await response.json();
+                    setData(jsonData);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [eingabe]); // useEffect abhängig von 'eingabe'
+
+    const handleSubmitEingabe = () => {
+        console.log("HAllo WELT")
+    }
+
+    return (
+        <div>
+            <h1>Abfrage Supporter</h1>
+            {data && (
+                <div>
+                    <p>Andere Daten: {JSON.stringify(data)}</p>
+                </div>
+            )}
+
+            <div onSubmit={handleSubmitEingabe}>
+                <input
+                    type="text"
+                    placeholder="ID"
+                    value={eingabe} // value statt onChange
+                    onChange={(e) => setEingabe(e.target.value)}
+                />
+                <button type={"submit"}>Check</button>
+
+            </div>
+                    </div>
+    );
+};
+
 export default App;
