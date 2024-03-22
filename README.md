ae3ccada355785cd5a89926229c1fea06cdf6397
Pascal <pbienenstein@posteo.de>
2024-03-22 12:41:14 +0100

    dayjs added. Sorting date and time. Buttons for testing implemented. CSS file added. Documentation updated.

--- README.md
+++ README.md
@@ -1,12 +1,63 @@
 # Abfrage Supporter
 ## App erstellen
-+ Git -> neues leeres Repository erstellen -> local clonen
-+ Lokalen Ordner in Terminal öffnen.
-  + Initialisiert neues Node.js Projekt: `yarn init -y` 
-  + Installiert create-react local auf dem System: `yarn global add create-react-app` 
-  + React App mit Template Typescript erstellen: `yarn create react-app abfrage-supporter --template typescript`
++ Git -> Neues leeres Repository erstellen -> Lokal klonen.
++ Lokalen Ordner im Terminal öffnen.
+  + Initialisiert ein neues Node.js-Projekt: `yarn init -y`
+  + Installiert create-react-app lokal auf dem System: `yarn global add create-react-app`
+  + React-App mit Template Typescript erstellen: `yarn create react-app abfrage-supporter --template typescript`
 
 ## React aufräumen
-+ alle CSS und Test datein entfernen
-+ App.tsx zu Arrow Function und FC hinzugefügt
-+ index.html Dokumenten Titel angepasst
++ Alle CSS- und Testdateien entfernen.
++ App.tsx zu Arrow Function und FC hinzugefügt.
++ Titel des index.html-Dokuments angepasst.
+
+## Implementierung
+### Abfrage
+
+### Ausgabe
+![ersteAusgabe.png](src%2Fimages%2Fdokumentation%2FersteAusgabe.png)  
+Erste Abfrage. Daten werden als JSON String ausgegeben.
+### Sortieren
++ Sortieren nach Datum
++ Sortieren nach Uhrzeit
+  Zuerst findet eine Sortierung nach Datum statt. Zusätzlich, um die Reihenfolge der Schichten einzuhalten, wird noch nach der Uhrzeit sortiert. So wird an Tagen mit zwei Schichten zuerst die frühere und anschließend die spätere Schicht angezeigt.
+
+### Übersetzen
+Für die Übersetzung wurde ein Array erzeugt, in dem der englische Name wie in der API gespeichert ist und die deutsche Übersetzung gespeichert sind:
+
+bike: "Fahrrad",  
+bottle: "Flasche",  
+island: "Insel",  
+keys: "Schlüssel",  
+kitchen: "Küche",  
+mic: "Mikro",  
+mobile: "Mobile Zellen",  
+sofa: "Sofa",  
+star: "Stern",  
+sun: "Sonne",  
+tent: "Zelt",  
+tree: "Baum",  
+turtle: "Turtle",  
+window: "bitte erfragen"
+
+## Test
+
+### Codes zum Testen:
+0492131A757780  
+04ABE51A757780  
+048DCC1A757780  
+04E0FD1A757780  
+049E0D1A757784  
+0433541A757780  
+04BB2B6ABE6F80  
+
+
+### Test Button
+Um unterschiedliche Fälle zu testen, wurden mehrere Buttons erstellt. Jeder Button wurde mit einer Beispiel-ID versehen. Über das Aufrufen der Buttons ist es möglich, Testabfragen zu machen.  
+![test1.png](src%2Fimages%2Fdokumentation%2Ftest1.png)  
+Auf dem Bild sind die Buttons mit entsprechender ID zu sehen.
+
+### Test Sortieren
+![test2.png](src%2Fimages%2Fdokumentation%2Ftest2.png)  
+Am 30.06.2023 sind zwei Schichten eingetragen. Zuerst wird die frühe Schicht ausgegeben.
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import React, { FC, useState } from 'react';
+import dayjs from "dayjs";
+import './css/App.css'
 
 const App: FC = () => {
+    //data und eingabe werden deklariert
     const [data, setData] = useState<any>(null);
     const [eingabe, setEingabe] = useState<string>("");
 
+    //Array mit übersetzungen zu den Treffpunkten wird deklariert
+    const waitingSpot : Record<string, string> = {
+        bike: "Fahrrad",
+        bottle: "Flasche",
+        island: "Insel",
+        keys: "Schlüssel",
+        kitchen: "Küche",
+        mic: "Mikro",
+        mobile: "Mobile Zellen",
+        sofa: "Sofa",
+        star: "Stern",
+        sun: "Sonne",
+        tent: "Zelt",
+        tree: "Baum",
+        turtle: "Taube",
+        window: "bitte erfragen"
+    }
 
     const fetchData = async () => {
         try {
-            if (eingabe.trim() !== "") { // Überprüfung, ob 'eingabe' leer ist
+            if (eingabe.trim() !== "") { // Überprüfung, ob eingabe leer ist
                 const ausgabe = await fetch(`https://supporter.kulturkosmos.de/api/self-service/shifts/${eingabe}`);
                 if (!ausgabe.ok) {
                     throw new Error('Fehler');
                 }
                 const jsonData = await ausgabe.json();
                 setData(jsonData);
-                console.log(jsonData);
+
             }
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -24,18 +44,23 @@ const App: FC = () => {
     const handleClick = () => {
         fetchData();
     };
+console.log(data);
 
     return (
         <div>
             <h1>Abfrage Supporter {eingabe}</h1>
-            {data && (
-                <div>
-                    <p>Andere Daten: {JSON.stringify(data)}</p>
-                    <p>Test 1: {data.startAt}</p>
-                    {/*<p>{data}</p>*/}
-                </div>
-            )}
+            {data && data.data
+                .sort((a: any, b: any) => dayjs(a.startAt).unix() - dayjs(b.startAt).unix())
+                .map((entry: any, index: number) => (
+                    <div key={index}>
+                        <p><strong>Zeit: </strong>{dayjs(entry.startAt).locale('de').format('DD.MM.YYYY HH:mm')}</p>
+                        <p><strong>Treffpunkt: </strong>{waitingSpot[entry.waitingSpot]}</p>
+                        <br />
+                    </div>
+                ))
+            }
 
+            {/*Wird später durch NFC-Reader eingabe ersetzt*/}
             <div>
                 <input
                     type="text"
@@ -44,10 +69,40 @@ const App: FC = () => {
                     onChange={(e) => setEingabe(e.target.value)}
                 />
                 <button onClick={handleClick}>Abfragen</button>
+                <button onClick={() => window.location.reload()}>Reset</button>
+            </div>
+
+            {/*Kann später alles raus.*/}
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("0492131A757780")}>0492131A757780</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("04ABE51A757780")}>04ABE51A757780</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("048DCC1A757780")}>048DCC1A757780</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("04E0FD1A757780")}>04E0FD1A757780</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("049E0D1A757784")}>049E0D1A757784</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("0433541A757780")}>0433541A757780</button>
+            </div>
+            <br />
+            <div>
+                <button onClick={()=> setEingabe("04BB2B6ABE6F80")}>04BB2B6ABE6F80</button>
             </div>
         </div>
     );
 };
 
 export default App;
-
